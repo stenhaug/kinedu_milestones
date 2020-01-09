6e7cf208ddacb1db0c2184d4d0fcdd86a56eead1
Ben Stenhaug <benastenhaug@gmail.com>
2020-01-08 17:47:25 -0800

    added in the interpretation section - great!

--- scratch.R
+++ scratch.R
@@ -15,9 +15,9 @@ d_mat <- d_mat[row.names(d_mat) %in% ages$id, ]
 
 d <- d %>% filter(age > 1)
 
-d_mat %>% apply(1, function(x) mean(is.na(x))) %>% table()
+# d_mat %>% apply(1, function(x) mean(is.na(x))) %>% table()
 
-d_mat %>% apply(2, function(x) mean(is.na(x))) %>% table()
+# d_mat %>% apply(2, function(x) mean(is.na(x))) %>% table()
 
 milestones <-
     d_raw %>%
@@ -49,6 +49,22 @@ areas %>%
         "Complains when their activity is interrupted"
     )) %>%
     View()
+
+areas %>%
+    filter(paste %in% colnames(d_mat)) %>%
+    group_by(area) %>%
+    mutate(count = n()) %>%
+    slice(1) %>%
+    ungroup() %>%
+    left_join(ms, by = c("paste" = "code")) %>%
+    select(group = area, count, spanish = short_name) %>%
+    arrange(desc(count)) %>%
+    mutate(english = c(
+        "Can stand on their toes",
+        "Can find an object on the floor",
+        "Babbles as if imitating conversations",
+        "Complains when their activity is interrupted"
+    ))
 
 mod3 <- mirt(d_mat, 3, "2PL")
 
